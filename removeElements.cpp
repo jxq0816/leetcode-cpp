095214c7825300a74c440deb2ed46dc98039ba8b
姜兴琪 <xingqijiang@qq.com>
2021-02-06 11:42:13 +0800

    No.203 移除链表元素

--- removeElements.cpp
+++ removeElements.cpp
@@ -16,15 +16,23 @@ public:
         ListNode* sentinel=new ListNode(0);
         sentinel->next=head;
         ListNode* pre=sentinel;
+        ListNode* toDelete = nullptr;
         while(head){
             if(head->val==val){
+                toDelete=head;
                 pre->next=head->next;
+
                 head=head->next;
                 continue;
             }
             pre=head;
             head=head->next;
+            if(toDelete!= nullptr){
+                delete toDelete;
+                toDelete=nullptr;
+            }
         }
+
         return sentinel->next;
     }
 };
